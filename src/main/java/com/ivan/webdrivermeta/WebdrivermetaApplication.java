d57c929090949b814c47e48d1971909589108383
jan <ivan.stseoaniuk@gmail.com>
2023-09-05 12:47:26 +0200

    -Introduced property; -Itroduced chrome client implementation(raw);

--- src/main/java/com/ivan/webdrivermeta/WebdrivermetaApplication.java
+++ src/main/java/com/ivan/webdrivermeta/WebdrivermetaApplication.java
@@ -4,14 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class WebdrivermetaApplication {
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
 	}
+//	@Bean
+//	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+//		return new PropertySourcesPlaceholderConfigurer();
+//	}
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebdrivermetaApplication.class, args);
--- src/main/java/com/ivan/webdrivermeta/ports/chrome/ChromeClient.java
+++ src/main/java/com/ivan/webdrivermeta/ports/chrome/ChromeClient.java
@@ -1,22 +1,34 @@
 package com.ivan.webdrivermeta.ports.chrome;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Component
 public class ChromeClient {
 
-    public ChromeClient(RestTemplate restTemplate) {
+    public ChromeClient(RestTemplate restTemplate,
+                        @Value("${chrome.api.url}") String serviceUrl) {
         this.restTemplate = restTemplate;
+        this.serviceUrl = serviceUrl;
     }
 
     private RestTemplate restTemplate;
+    private String serviceUrl;
 
     public String getDriverUrl(String ver, String platform) {
-        String serviceUrl = "https://googlechromelabs.github.io/chrome-for-testing/latest-versions-per-milestone-with-downloads.json";
         Milestones res = restTemplate.getForObject(
-                serviceUrl, Milestones.class);
-        return "eqwqe";
+                this.serviceUrl, Milestones.class);
+        List<Download> resList = res.milestones().get(ver).downloads().get("chrome");
+        for (Download d: resList) {
+            if (d.platform() != null && d.platform().equals(platform)){
+                return d.url();
+            }
+
+        }
+        throw new RuntimeException("download url not found");
     }
 }
