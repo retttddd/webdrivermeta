ebdc2365857a0d8a05af3fa8330234e11b8b0905
jan <ivan.stseoaniuk@gmail.com>
2023-09-08 15:29:57 +0200

    - provided used port usage avoidance;

--- src/main/java/com/ivan/webdrivermeta/WebdrivermetaApplication.java
+++ src/main/java/com/ivan/webdrivermeta/WebdrivermetaApplication.java
@@ -1,13 +1,18 @@
 package com.ivan.webdrivermeta;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Random;
 
 @SpringBootApplication
 @EnableCaching
@@ -22,6 +27,17 @@ public class WebdrivermetaApplication {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> {
+            int minPort = 8080;
+            int maxPort = 65535;
+            int randomPort = new Random().nextInt(maxPort - minPort + 1) + minPort;
+
+            factory.setPort(randomPort);
+        };
     }
 
 }
--- src/main/java/com/ivan/webdrivermeta/controller/DriverMetaController.java
+++ src/main/java/com/ivan/webdrivermeta/controller/DriverMetaController.java
@@ -38,4 +38,5 @@ public class DriverMetaController {
             super(message);
         }
     }
+
 }
